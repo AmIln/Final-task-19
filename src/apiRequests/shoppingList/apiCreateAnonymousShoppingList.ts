1f19c67a9c41169854b31976ec64c50f531c62e1
Dmitry Andriyanov <facklak@mail.ru>
2024-06-21 15:07:30 +0300

    feat: added createCart function

--- src/apiRequests/shoppingList/apiCreateAnonymousShoppingList.ts
+++ src/apiRequests/shoppingList/apiCreateAnonymousShoppingList.ts
@@ -1,6 +1,7 @@
 import { getProjectHost } from "../../helpers/getsAPI";
 import { ShoppingList } from "../../helpers/interfaces/ShoppingList";
 import { apiGetShoppingList } from "./apiGetShoppingList";
+import { createCart } from "./createCart";
 
 export async function apiCreateAnonymousShoppingList(): Promise<ShoppingList | void> {
   const myHeaders = new Headers();
@@ -40,6 +41,8 @@ export async function apiCreateAnonymousShoppingList(): Promise<ShoppingList | v
     const result = await response.text();
     const json = JSON.parse(result) as ShoppingList;
     sessionStorage.setItem("basketKey", `Anonymous-${token}-shopping-list`);
+    // создаем корзину
+    await createCart();
     return json;
   } catch (error) {
     console.log(error);
