2f9330a1c1410d5970be9fe76e65b7178e02cfd8
Dmitry Andriyanov <facklak@mail.ru>
2024-06-24 23:28:42 +0300

    feat: improved the code emptyBasketPageCreator function

--- src/pages/basket/emptyBasketPage.ts
+++ src/pages/basket/emptyBasketPage.ts
@@ -1,24 +1,17 @@
 import { getCart } from "../../apiRequests/shoppingList/getCart";
+import { createElement } from "../../helpers/creators/createElement";
 import { route } from "../../router/route";
 
 export function emptyBasketPageCreator() {
   const content = document.querySelector(".content") as HTMLDivElement;
   content.innerHTML = "";
 
-  const messageWraper = document.createElement("div");
-  messageWraper.classList.add("empty_basket_message_wraper");
+  const messageWraper = createElement("div", "empty_basket_message_wraper");
+  const mainText = createElement("div", "empty_basket_main_text", "Your bag is empty");
+  const secondText = createElement("div", "empty_basket_second_text", "Start shopping for awesome gifts");
 
-  const mainText = document.createElement("div");
-  mainText.innerHTML = `Your bag is empty`;
-  mainText.classList.add("empty_basket_main_text");
-
-  const secondText = document.createElement("div");
-  secondText.classList.add("empty_basket_second_text");
-  secondText.innerHTML = `Start shopping for awesome gifts`;
-
-  const button = document.createElement("div");
-  button.classList.add("button_to_cataloge_page", "nav__item");
-  button.innerHTML = `Go to catalog page`;
+  const button = createElement("div", "button_to_cataloge_page", "Go to catalog page");
+  button.classList.add("nav__item");
   button.addEventListener("click", (event) => {
     event.preventDefault();
     route("");
@@ -29,12 +22,11 @@ export function emptyBasketPageCreator() {
 }
 
 export async function isEmptyBasket(): Promise<boolean> {
-  const cartId = sessionStorage.getItem("cartId");
-  if (!cartId) return false;
   const cart = await getCart();
 
   if (cart && cart.lineItems.length > 0) {
     return false;
   }
+
   return true;
 }
