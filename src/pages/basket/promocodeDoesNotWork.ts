5b6a5313caf5aefb64936f26c457b71ebf840a03
Dmitry Andriyanov <facklak@mail.ru>
2024-06-24 23:21:10 +0300

    feat: delete console.log

--- src/pages/basket/promocodeDoesNotWork.ts
+++ src/pages/basket/promocodeDoesNotWork.ts
@@ -1,7 +1,6 @@
 import { createNotification } from "../../notification/createNotificationElem";
 
 export async function promocodeDoesNotWork(wrapper: HTMLElement, price: number): Promise<void> {
-  console.log("promocodeDoesNotWork");
   wrapper.innerHTML = `The total cost is: ${Math.round(price) / 100} $`;
   createNotification("info", "Promo code does not match your cart");
   const discontInput = document.getElementById("discont_input") as HTMLInputElement;
