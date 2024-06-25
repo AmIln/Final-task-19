dd834bb4929296a48d66209c9ebcab7d2c188aae
Dmitry Andriyanov <facklak@mail.ru>
2024-06-25 22:47:17 +0300

    fix: delete createCart from this functions, moved to createBasket function

--- src/apiRequests/shoppingList/apiCreateShoppingList.ts
+++ src/apiRequests/shoppingList/apiCreateShoppingList.ts
@@ -2,9 +2,8 @@ import { getProjectHost } from "../../helpers/getsAPI";
 import { ShoppingList } from "../../helpers/interfaces/ShoppingList";
 import { getCustomerById } from "../getCustomerById";
 import { apiGetShoppingList } from "./apiGetShoppingList";
-import { createCart } from "./createCart";
 
-export async function apiCreateShoppingList(customerId: string): Promise<ShoppingList | null> {
+export async function apiCreateShoppingList(customerId: string): Promise<ShoppingList | false> {
   const myHeaders = new Headers();
   const token = sessionStorage.getItem("token");
   const tokenType = sessionStorage.getItem("token-type");
@@ -46,12 +45,9 @@ export async function apiCreateShoppingList(customerId: string): Promise<Shoppin
     const result = await response.text();
     const json = JSON.parse(result) as ShoppingList;
 
-    // create Cart
-    await createCart();
-
     return json;
   } catch (error) {
     console.log(error);
-    return null;
+    return false;
   }
 }
